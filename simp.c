a540f63d7d51b27139e184fe67bbeb1fa7856265
Jojo-muri <gmuriuki281@gmail.com>
2024-09-27 10:35:58 +0400

    Add files via upload

---
+++ simp.c
@@ -0,0 +1,13 @@
+// program to find simple intrest
+/*
+name ;george waweru
+adm ;CT101/G/23698/24
+date ;27/9/2024
+*/
+#include<stdio.h>
+int main() {
+	int P =20000 , R = 4 , T = 2 ;
+	int SI = ( P * R * T/100) ;
+	printf ("the simple intrest is : , %d" , SI) ;
+	return 0 ;
+}
